0ec3255c464cb763bf8369dc8eb934761e52fbc9
Lara Kamper <lailacaty@Mac.fritz.box>
2025-06-12 08:57:00 +0200

    added: index template

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
 from flask import Flask
+from flask import render_template
+import json
 
 app = Flask(__name__)
 
+# Function to load posts from the JSON file
+def load_posts():
+    with open('data.json', 'r') as file:
+        posts = json.load(file)
+    return posts
 
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def index():
+    blog_posts = load_posts()
+    return render_template('index.html', posts=blog_posts)
+
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5002, debug=True)
